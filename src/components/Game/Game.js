1e99126c2f5dbb9709d7baebb61183b4d727e10b
kkauma <kylekauma@gmail.com>
2024-10-06 12:27:10 -0700

    create GuessInput component and boilerplate form

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from "react";
 
-import { sample } from '../../utils';
-import { WORDS } from '../../data';
+import { sample } from "../../utils";
+import { WORDS } from "../../data";
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
